56915083e3c1040796687ae49ca62fd67817a64e
manik1312006 <manikmehtaji@gmail.com>
2024-10-22 16:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Inspect Blocker
+
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
 // Function to embed a video
 function embedVideo() {
     const link = document.getElementById('videoLink').value;
