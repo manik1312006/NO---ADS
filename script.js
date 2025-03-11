7b8aa5071735b73e183e15e889e16c2bd371b966
manik1312006 <manikmehtaji@gmail.com>
2025-03-12 00:12:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function embedVideo() {
 
     if (videoId) {
         const iframe = document.createElement("iframe");
-        iframe.src = `https://www.youtube.com/embed/${videoId}`;
+        iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&enablejsapi=1`;
         iframe.setAttribute("allowfullscreen", "true");
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
@@ -47,54 +47,10 @@ function extractVideoID(url) {
     return match ? match[1] : null;
 }
 
-// Function to embed a playlist
-function embedPlaylist() {
-    const link = document.getElementById("videoLink").value;
-    const playlistId = extractPlaylistID(link);
-    const videoContainer = document.getElementById("videoContainer");
-    videoContainer.innerHTML = ""; // Clear previous content
-
-    if (playlistId) {
-        const iframe = document.createElement("iframe");
-        iframe.src = `https://www.youtube.com/embed/videoseries?list=${playlistId}`;
-        iframe.setAttribute("allowfullscreen", "true");
-        iframe.setAttribute("frameborder", "0");
-        iframe.width = "858";
-        iframe.height = "429";
-        videoContainer.appendChild(iframe);
-    } else {
-        alert("Please enter a valid YouTube playlist link.");
-    }
-}
-
-// Function to extract playlist ID
-function extractPlaylistID(url) {
-    const regex = /[?&]list=([a-zA-Z0-9_-]+)/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-}
-
-// Auto-enable PiP when app is minimized or screen is turned off
-document.addEventListener("visibilitychange", async () => {
-    let iframe = document.querySelector("iframe");
-
-    if (document.hidden && iframe) {
-        try {
-            let videoDocument = iframe.contentDocument || iframe.contentWindow.document;
-            let video = videoDocument.querySelector("video");
-
-            if (video && document.pictureInPictureEnabled && !document.pictureInPictureElement) {
-                await video.requestPictureInPicture();
-            }
-        } catch (err) {
-            console.error("Error enabling PiP:", err);
-        }
-    } else if (!document.hidden && document.pictureInPictureElement) {
-        try {
-            await document.exitPictureInPicture();
-        } catch (err) {
-            console.error("Error exiting PiP:", err);
-        }
+// Function to auto-prompt user for PiP on Android when minimizing
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+        alert("To continue watching, manually enable Picture-in-Picture mode from your browser.");
     }
 });
 
@@ -103,4 +59,4 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js")
         .then(() => console.log("Service Worker Registered"))
         .catch((error) => console.log("Service Worker Registration Failed", error));
-});
+}
